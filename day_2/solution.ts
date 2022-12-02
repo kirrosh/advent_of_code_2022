71185152e5499265598be13076c39b046a03727e
Koshelev Kirill <olegartor7@gmail.com>
2022-12-02 13:52:06 +0400

    fix: day_2 ts solution

--- day_2/solution.ts
+++ day_2/solution.ts
@@ -1,25 +1,16 @@
 import { assertEquals } from "https://deno.land/std@0.166.0/testing/asserts.ts";
 type Default = "A" | "B" | "C";
 type Cusom = "X" | "Y" | "Z";
-const VALUES = {
-  A: 1,
-  B: 2,
-  C: 3,
-};
 
 class Node {
   value: number;
   beat: Node | null = null;
-  // loose: Node | null = null;
   constructor(value: number) {
     this.value = value;
   }
   setBeat(node: Node) {
     this.beat = node;
   }
-  // setLoose(node: Node) {
-  //     this.loose = node;
-  // }
   compare(node: Node) {
     if (this.value === node.value) {
       return 3;
@@ -36,61 +27,70 @@ const createRools = () => {
   const B = new Node(2);
   const C = new Node(3);
   A.setBeat(C);
-  // A.setLoose(B);
   B.setBeat(A);
-  // B.setLoose(C);
   C.setBeat(B);
-  // C.setLoose(A);
   return { A, B, C };
 };
 
-const rotation = function* () {
-  const { A, B, C } = createRools();
-  yield { X: A, Y: B, Z: C };
-  yield { X: B, Y: C, Z: A };
-  yield { X: C, Y: A, Z: B };
-
-  yield { X: A, Y: C, Z: B };
-  yield { X: B, Y: A, Z: C };
-  yield { X: C, Y: B, Z: A };
-};
-
-const calculateAll = (input: [Default, Cusom][]) => {
-  const rools = createRools();
-
-  let sums = [];
-  for (const custom of rotation()) {
-    let sum = 0;
-    input.forEach((item) => {
-      const [a, b] = item;
-      const aNode = rools[a as Default];
-      const bNode = custom[b as Cusom];
-      sum += bNode.compare(aNode) + bNode.value;
-    });
-    sums.push(sum);
-  }
-  return sums;
-};
-
 const solution = (input: [Default, Cusom][]): number => {
-  const sums = calculateAll(input);
-  return sums[0];
-  // Do not need  to find Max LOl
-  //   return Math.max(...sums);
+  const rools = createRools();
+  const { A, B, C } = rools;
+  const custom = { X: A, Y: B, Z: C };
+  let sum = 0;
+  input.forEach((item) => {
+    const [a, b] = item;
+    const aNode = rools[a as Default];
+    const bNode = custom[b as Cusom];
+    sum += bNode.compare(aNode) + bNode.value;
+  });
+  return sum;
 };
 
-Deno.test("Test", () => {
+const solutionPart_2 = (input: [Default, Cusom][]): number => {
+  const rools = createRools();
+  let sum = 0;
+  input.forEach((item) => {
+    const [a, b] = item;
+    const aNode = rools[a as Default];
+    if (b == "X") {
+      sum += aNode.beat?.value! + 0;
+    } else if (b == "Y") {
+      sum += aNode.value + 3;
+    } else {
+      sum += aNode.beat?.beat?.value! + 6;
+    }
+  });
+  return sum;
+};
+
+Deno.test("Part 1", () => {
   const res = solution([
     ["A", "Y"],
     ["B", "X"],
     ["C", "Z"],
   ]);
-  assertEquals(res, 13268);
+  assertEquals(res, 15);
 });
 
-Deno.test("Input", async () => {
+Deno.test("Part 1 Input", async () => {
   const text = await Deno.readTextFile("./input.txt");
   const parsed = text.split("\r\n").map((line) => line.split(" "));
   const res = solution(parsed as [Default, Cusom][]);
   assertEquals(res, 13268);
+});
+
+Deno.test("Part 2", () => {
+  const res = solutionPart_2([
+    ["A", "Y"],
+    ["B", "X"],
+    ["C", "Z"],
+  ]);
+  assertEquals(res, 12);
+});
+
+Deno.test("Part 2 Input", async () => {
+  const text = await Deno.readTextFile("./input.txt");
+  const parsed = text.split("\r\n").map((line) => line.split(" "));
+  const res = solutionPart_2(parsed as [Default, Cusom][]);
+  assertEquals(res, 15508);
 });
